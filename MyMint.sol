dc5070b55a211741a250a21f14b0cd014a0d85c8
Christian1969-429 <cniczyporuk@student.uaustin.org>
2025-06-10 21:37:39 -0500

    Update MyMint.sol to be accurate and work properly

--- MyMint.sol
+++ MyMint.sol
@@ -4,31 +4,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyMint is ERC20 {
 
-    string tokenName= "My minting smart contract."; // You set the name.
-    string tokenSymbol= "MINT"; // You set the symbol.
-    uint256 tokenDecimals= 18; // Most tokens use 18 decimal places.
-    uint256 maxTokenSupply= 1000000*(10**tokenDecimals); // You set the max supply.
+    string tokenName = "GEARcoin";
+    string tokenSymbol = "GAS";
+    uint256 tokenDecimals = 18;
+    uint256 maxTokenSupply = 9000000 * (10 ** tokenDecimals);
 
-    constructor() ERC20(tokenName, tokenSymbol) { 
-        /* This function creates the token, but it does not mint any tokens.
-            The minting is done by a separate function below. */     
-    }
-    
+    constructor() ERC20(tokenName, tokenSymbol) {}
+
     function mintFunction(uint256 amountToMint) public {
+        // Enforce min and max minting limits
+        require(amountToMint >= 5, "You must mint at least 5 tokens.");
+        require(amountToMint <= 100000, "You cannot mint more than 100,000 tokens at a time.");
 
-        // Amount is submitted as a whole number, but _mint() function is in decimals. 
-        uint256 amountInDecmials = amountToMint*10**tokenDecimals;
+        uint256 amountInDecimals = amountToMint * (10 ** tokenDecimals);
+        uint256 currentSupply = totalSupply();
 
-        // check that current supply plus new mint amount < token supply 
-        uint256 currentSupply = totalSupply(); // Gets the current supply of this token.
-        
-        // If the new mint would exceed maxTokenSupply, the return an error message.
-        require(currentSupply + amountToMint <= maxTokenSupply, 
-            "Minting the amount requested would exceed the max sypply allowed.") ;
+        require(currentSupply + amountInDecimals <= maxTokenSupply, 
+            "Minting the amount requested would exceed the max supply allowed.");
 
-        /* You could add more requirements here like a restricting who can mint tokens
-            or the amount they are allowed to mint. */
-
-        _mint(msg.sender, amountInDecmials);
+        _mint(msg.sender, amountInDecimals);
     }
 }
